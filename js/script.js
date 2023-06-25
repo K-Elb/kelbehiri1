a4b1b60791ea9b6585a059699cf94e95863d2606
K-Elb <67399224+K-Elb@users.noreply.github.com>
2023-06-26 03:03:44 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
- // When the user scrolls the page, execute pageScroll function 
- window.onscroll = function() {
-  pageScroll();
-};
 
-function pageScroll() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("proBar").style.width = scrolled + "%";
-}
 
 // Fancy word effect
 
@@ -80,34 +70,43 @@ enhance("channel-link");
 //   }, 30);
 // }
 
-//Get the button
-
-let mybutton = document.getElementById("btn-back-to-top");
-let navbar = document.getElementById("navScroll");
-
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+window.onscroll = function() {
+  // When the user scrolls the page, execute pageScroll function 
+  pageScroll();
+  // When the user scrolls down 20px from the top of the document, show the button
   scrollFunction();
 };
+
+function pageScroll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("proBar").style.width = scrolled + "%";
+}
+
+//Get to the top button
+
+let mybutton = document.getElementById("btn-back-to-top");
+// let navbar = document.getElementById("navScroll");
 
 function scrollFunction() {
   if (
     document.body.scrollTop > 1000 ||
     document.documentElement.scrollTop > 1000
-    ) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  // When the user clicks on the button, scroll to the top of the document
-  mybutton.addEventListener("click", backToTop);
-  
-  function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-  
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
   
   
